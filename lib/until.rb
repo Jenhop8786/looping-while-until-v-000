c520673b74f16fb45ad6e2e03251256265c460e0
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-11-11 00:28:19 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,11 +2,9 @@
 
 
 def using_until
-  levitation_force = 6 + 1 
-  puts "Wingardium Leviosa" 
+  levitation_force = 6 + 1
   puts "Wingardium Leviosa"
   puts "Wingardium Leviosa"
   puts "Wingardium Leviosa"
-  
-
+  puts "Wingardium Leviosa"
 end
