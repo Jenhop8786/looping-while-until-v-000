6701d7a9221b4253c33fdace364bf6e08ccfcc47
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-11-11 00:26:55 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,8 @@
-def using_until
-        levitation_force = 6
-        #your code here
-    
-end
 
+
+phrase = "Windgardium Leviosa"
+def using_until
+  puts "Wingardium Leviosa" until 0 < 10
+  levitation_force = 6 + 1 
+
+end
